3174a27edb1d40c479858560d0e27468538a5bc6
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-11-07 12:18:24 +0100

    Commit 2

--- lib/screens/first_onboarding_screen.dart
+++ lib/screens/first_onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/screens/second_onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
 class FirstOnBoardingScreen extends StatefulWidget {
@@ -11,10 +12,15 @@ class FirstOnBoardingScreen extends StatefulWidget {
 
 class _FirstOnBoardingScreenState extends State<FirstOnBoardingScreen> {
 
- final imageOffset = Offset.zero;
+ Offset imageOffset = Offset.zero;
+  String titleText = "Deliver";
+
+  double buttonOffset = 0.0;
+
 
   @override
   Widget build(BuildContext context) {
+    double buttonWidht = MediaQuery.of(context).size.width - 80;
     return Scaffold(
       backgroundColor: Colors.teal,
       body: Column(
@@ -22,7 +28,7 @@ class _FirstOnBoardingScreenState extends State<FirstOnBoardingScreen> {
           Spacer(),
           Center(
             child: Text(
-              'Deliver',
+              titleText,
               style: TextStyle(
                 fontSize: 60,
                 fontWeight: FontWeight.bold,
@@ -42,64 +48,113 @@ class _FirstOnBoardingScreenState extends State<FirstOnBoardingScreen> {
           ),
           Spacer(),
           Transform(
-            transform: Matrix4.translationValues(imageOffset.dx,0,0),
-            child: SizedBox(
-              width: 350,
-              height: 350,
-              child: Image.asset('assets/character-1.png'),
+            transform: Matrix4.translationValues(
+                imageOffset.dx,
+                0,
+                0
+            )..rotateZ((imageOffset.dx /20) * 3.14 / 180),
+            child: GestureDetector(
+              onPanUpdate: (details){
+                if (imageOffset.dx.abs() <= 128){
+                  setState(() {
+                    imageOffset = Offset(imageOffset.dx - details.delta.dx, 0);
+                    titleText = "Order";
+                  });
+                }
+              },
+              onPanEnd: (_){
+                setState(() {
+                  imageOffset = Offset.zero;
+
+                  titleText = "Deliver";
+                });
+              },
+              child: SizedBox(
+                width: 350,
+                height: 350,
+                child: Image.asset('assets/character-1.png'),
+              ),
             ),
           ),
           Spacer(),
-          Container(
-            width: MediaQuery.of(context).size.width - 80,
-            height: 88,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.2),
-              borderRadius: BorderRadiusDirectional.circular(40),
-            ),
-            child: Stack(
-              children: [
-                Container(
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.red.shade200,
-                    borderRadius: BorderRadius.circular(40),
+          GestureDetector(
+            onPanUpdate: (details){
+              if(details.delta.dx > 8 && buttonOffset <= buttonWidht - 80){
+                setState(() {
+                  buttonOffset += details.delta.dx;
+                });
+              }
+            },
+            onPanEnd: (_){
+              if (buttonOffset > buttonWidht / 2){
+                setState(() {
+                  buttonOffset = buttonWidht - 80;
+                });
+                print("Working");
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondOnBoardingScreen()
+                    ),
+                );
+              } else{
+                buttonOffset = 0.0;
+              }
+
+              },
+            child: Container(
+              width: buttonWidht,
+              height: 88,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadiusDirectional.circular(40),
+              ),
+              child: Stack(
+                children: [
+                  AnimatedContainer(
+                    width: buttonOffset + 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.red.shade200,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    duration: Duration(
+                      milliseconds: 200
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        width: 80,
+                        height: 70,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.chevron_right,
+                            size: 40,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  child: Align(
+                  Align(
                     alignment: Alignment.center,
                     child: Container(
-                      width: 80,
-                      height: 70,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.red,
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.chevron_right,
-                          size: 40,
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Get Started',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Spacer(),
